6527bdedd53c1e1538987b377994e4cc3d920240
rossini rodrigues alves <rossini_rodrigues_alves@hotmail.com>
2022-01-22 22:15:41 -0300

    CRUD de médicos e especialidades

---
+++ src/Controller/MedicosController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Medico;
+use App\Helper\MedicoFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MedicosController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var MedicoFactory
+     */
+    private $medicoFactory;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        MedicoFactory $medicoFactory,
+        MedicosRepository $medicosRepository
+    ) {
+        $this->entityManager = $entityManager;
+        $this->medicoFactory = $medicoFactory;
+        $this->medicosRepository = $medicosRepository;
+    }
+
+    /**
+     * @Route("/medicos", methods={"POST"})
+     */
+    public function novo(Request $request): Response
+    {
+        $corpoRequisicao = $request->getContent();
+        $medico = $this->medicoFactory->criarMedico($corpoRequisicao);
+
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
+
+        return new JsonResponse($medico);
+    }
+
+    /**
+     * @Route("/medicos", methods={"GET"})
+     */
+    public function buscarTodos(): Response
+    {
+       $medicoList = $this->$medicosRepository->findAll();
+
+        return new JsonResponse($medicoList);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     */
+    public function buscarUm(int $id): Response
+    {
+        $medico = $this->buscaMedico($id);
+        $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
+
+        return new JsonResponse($medico, $codigoRetorno);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"PUT"})
+     */
+    public function atualiza(int $id, Request $request): Response
+    {
+        $corpoRequisicao = $request->getContent();
+        $medicoEnviado = $this->medicoFactory->criarMedico($corpoRequisicao);
+
+        $medicoExistente = $this->buscaMedico($id);
+        if (is_null($medicoExistente)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $medicoExistente
+            ->setCrm($medicoEnviado->getCrm())
+            ->setNome($medicoEnviado->getNome());
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($medicoExistente);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"DELETE"})
+     */
+    public function remove(int $id): Response
+    {
+        $medico = $this->buscaMedico($id);
+        $this->entityManager->remove($medico);
+        $this->entityManager->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @param int $id
+     * @return object|null
+     */
+    public function buscaMedico(int $id)
+    {
+       
+        $medico = $medicosRepository->find($id);
+
+        return $medico;
+    }
+    /**
+     * @Route("/especialidades/{especialidadeId}/medicos", methods={"GET"})
+     */
+    public function buscaPorEspecialidade(int $especialidadeId): Response {
+       
+        $medicos = $medicosRepository->findBy([
+            'especialidade' => $especialidadeId
+        ]);
+
+        return new JsonResponse($medicos);
+    }
+}
